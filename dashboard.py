7810c92972af051fdb12d89695f798e5a611d5be
stickcointothemoon-alt <stickcointothemoon@gmail.com>
2025-11-12 20:22:15 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -277,4 +277,8 @@ def import_seen():
 
 # ----- Run -----
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
+    import os
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port)
+
+
